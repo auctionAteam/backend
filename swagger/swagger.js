37a888d5a17a9dc233f82677ea81836f567ac666
seoku9 <sukkoo777@gmail.com>
2025-06-09 10:41:59 +0900

    fix: swagger 설정 및 패키지 수정

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -7,17 +7,16 @@ const options = {
     info: {
       version: "1.0.1",
       title: "Auction API",
-      description:
-        `<h3>온라인 경매 사이트</h3>`
+      description: `<h3>온라인 경매 사이트</h3>`,
     },
     servers: [
       {
-        url: "http://localhost:5000", // 요청 URL
+        url: "http://localhost:3000", // 요청 URL
       },
     ],
   },
-  apis: ["./swagger/*.swagger.js"], 
-}
-const specs = swaggereJsdoc(options)
+  apis: ["./swagger/*.swagger.js"],
+};
+const specs = swaggereJsdoc(options);
 
-module.exports = { swaggerUi, specs }
+module.exports = { swaggerUi, specs };
